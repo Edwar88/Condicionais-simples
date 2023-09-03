feccdae77f2eaf15cc6fc558645b26746be5442e
Eduardo Cedro <eduardocsjr88@gmail.com>
2023-09-03 07:08:05 -0300

    Condicional-composta

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,26 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        
+        int nota = 8;
+
+        if(nota >= 7){
+            System.out.println("Aprovado");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
